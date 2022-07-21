d71ce1c20175d25eb9f2ce42cf4a335964ef120a
Osama Maqtari <almaktary.osama@gmail.com>
2022-07-22 00:28:29 +0300

    Fix buffer issue

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 
+// workaround: Buffer is not defined error
+window.Buffer = window.Buffer || require("buffer").Buffer;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
